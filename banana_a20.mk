65776448a1cde34fdcbf1c5596b6c951177cf9ce
GKart <gkartyt@gmail.com>
2022-10-14 10:21:33 -0300

    Banana Bringup

---
+++ banana_a20.mk
@@ -0,0 +1,6 @@
+$(call inherit-product, device/samsung/a20/full_a20.mk)
+$(call inherit-product, vendor/banana/config/common.mk)
+PRODUCT_NAME := banana_a20
+# Additional Props
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_BOOT_ANIMATION_RES := 720
